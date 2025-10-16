022b69b449e09024b7fee90506ca253ea928e1c9
Mosiah Adam <mosiah.adam@escolar.ifrn.edu.br>
2025-10-16 19:06:09 -0300

    feat: view para baixar documento

--- crawler/views.py
+++ crawler/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from .crawler import AbstractCrawler, Crawler
 
-# Create your views here.
+def index(request):
+    crawler: AbstractCrawler = Crawler(None)
+    crawler.crawl()
+    return HttpResponse("Arquivos salvos.")
