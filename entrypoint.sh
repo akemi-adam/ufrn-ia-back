77e3a10dfc31dc5d66a054c4d3ee412201493eca
Mosiah Adam <mosiah.adam@escolar.ifrn.edu.br>
2025-10-19 09:31:38 -0300

    config: rodar aplicação com uvicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ fi
 
 if [ -f manage.py ]; then
   echo "Iniciando servidor Django na porta 8000..."
-  exec python manage.py runserver 0.0.0.0:8000
+  exec python3 -m uvicorn ufrnia.asgi:application --host 0.0.0.0 --port 8000 --reload
 fi
 
 exec "$@"
