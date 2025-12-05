4c3c6997a2f77983b51e502932ab82f7c863e1c1
Felipe Erik <felipeerik1524@gmail.com>
2025-12-05 20:14:07 -0300

    feat: criação de serializers para registro e login

---
+++ users/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import User
+from django.contrib.auth.hashers import make_password, check_password
+
+class RegisterSerializer(serializers.Serializer):
+    name = serializers.CharField()
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+        validated_data["password"] = make_password(validated_data["password"])
+        user = User(**validated_data)
+        user.save()
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
