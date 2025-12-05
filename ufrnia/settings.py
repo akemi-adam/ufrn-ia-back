46f78a77f709cab920f107be2ee244a508c65841
Felipe Erik <felipeerik1524@gmail.com>
2025-12-05 19:49:02 -0300

    feat: configurar conexão com MongoDB no projeto

--- ufrnia/settings.py
+++ ufrnia/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
+from mongoengine import connect
+
+connect(db="ufrnia", host="mongodb://mongo:27017/ufrnia")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -88,8 +91,9 @@ ASGI_APPLICATION = "ufrnia.asgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.dummy'
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
