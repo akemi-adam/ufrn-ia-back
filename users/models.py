b201f484b0e751cc45c833691390015462be6bf4
Felipe Erik <felipeerik1524@gmail.com>
2025-12-05 20:10:47 -0300

    feat: implementar modelo User com mongoengine

--- users/models.py
+++ users/models.py
@@ -1,3 +1,28 @@
-from django.db import models
+from mongoengine import Document, StringField, EmailField, DateTimeField
+from datetime import datetime
 
-# Create your models here.
+class User(Document):
+    name = StringField(required=True, max_length=50)
+    email = EmailField(required=True, unique=True)
+    password = StringField(required=True, max_length=128)
+    created_at = DateTimeField(default=datetime.now)
+
+    meta = {
+        "collection": "users"
+    }
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_staff(self):
+        return False
+
+    @property
+    def is_superuser(self):
+        return False
