eabdb64d337abf45f1400978015f74bb1932d1bb
Felipe Erik <felipeerik1524@gmail.com>
2025-12-06 12:55:26 -0300

    feat: adição de model Message

--- messages/models.py
+++ messages/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from mongoengine import Document, StringField, ReferenceField, DateTimeField
+from users.models import User
+from chat.models import Chat
+from datetime import datetime
 
-# Create your models here.
+class Message(Document):
+    chat = ReferenceField(Chat, required=True)
+    sender = ReferenceField(User, required=False)
+    role = StringField(choices=["user", "ia"], required=True)
+    content = StringField(required=True)
+    created_at = DateTimeField(default=datetime.now)
