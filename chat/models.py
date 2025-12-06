00b4052251f0ae72b62e3f27d7a0e9663fb0305a
Felipe Erik <felipeerik1524@gmail.com>
2025-12-06 12:44:12 -0300

    feat: adição de modelo Chat

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,8 @@
-from django.db import models
+from mongoengine import Document, StringField, ReferenceField, DateTimeField, ListField
+from users.models import User
+from datetime import datetime
 
-# Create your models here.
+class Chat(Document):
+    user = ReferenceField(User, required=True)
+    title = StringField(required=True)
+    created_at = DateTimeField(default=datetime.now)
