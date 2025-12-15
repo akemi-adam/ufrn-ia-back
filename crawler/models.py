8b24d756012ae491f29510fc20502dcc5c1257b7
Felipe Erik <felipeerik1524@gmail.com>
2025-12-15 13:07:21 -0300

    feat: adição do model Collection

--- crawler/models.py
+++ crawler/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Collection(models.Model):
+    name = models.CharField(max_length=100)
+    url = models.URLField(unique=True)
+    last_scraping = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
